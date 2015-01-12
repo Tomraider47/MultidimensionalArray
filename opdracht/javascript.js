2f27e8260467d3a1d0b0019e8c4529732696373d
Koelekoetjes <Jens.haastrecht@hotmail.com>
2015-01-12 20:18:25 +0100

    Html/js/css
    HTML, Javascript en CSS files voor de multidimensionale arrays

---
+++ opdracht/javascript.js
@@ -0,0 +1,13 @@
+function links(){
+	var array = [
+		["Nu", "http://www.nu.nl"],
+		["Meewezen", "http://www.meewezen.nl"],
+		["IDCollege", "http://www.idcollege.nl"],
+		["Microsoft", "http://www.microsoft.nl"],
+		["De Telegraaf", "http://www.telegraaf.nl"]
+		]
+for (var i = 0; i < array.length; i++ ) {
+	document.write("<a href=" + array[i][1] + ">" + array[i][0] + "<br>")
+};
+
+}
